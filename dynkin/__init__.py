2acd4652a15af5fc3baf85fe0c3dd7bb1fbdcec9
freol35241 <freol@outlook.com>
2020-06-24 20:29:39 +0200

    Adding missing __init__-file

---
+++ dynkin/__init__.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def assert_3D_vector(v):
+    """
+    Make sure v is a 3x1 numpy array with dtype==float
+    """
+    out = np.array(v, dtype=float)
+    if not out.size == 3:
+        raise AssertionError('Must be a np.ndarray of size 3!')
+    return out
+
+def assert_6D_vector(v):
+    """
+    Make sure v is a 6x1 numpy array with dtype==float
+    """
+    out = np.array(v, dtype=float)
+    if not out.size == 6:
+        raise AssertionError('Must be a np.ndarray of size 6!')
+    return out
+
+def assert_6D_matrix(m):
+    """
+    Make sure m is a 6x6 numpy array with dtype==float
+    """
+    out = np.array(m, dtype=float)
+    if not out.shape == (6, 6):
+        raise ValueError('Must be a np.ndarray of shape (6,6)!')
+    return out
+
+
+from .frame import Frame, transform
+from .rigid_body import RigidBody
+
+__all__ = [
+    'RigidBody',
+    'Frame',
+    'transform'
+]
